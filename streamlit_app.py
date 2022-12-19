9587bc53a29faf25066c81fce932a5a0265e891b
Cindy Lacay <102972116+CindyLacay@users.noreply.github.com>
2022-12-19 13:07:53 +0800

    Update streamlit_app.py
    added normalized version

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# take the json version and normalized it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# tfor table output
+streamlit.dataframe(fruityvice_normalized)
